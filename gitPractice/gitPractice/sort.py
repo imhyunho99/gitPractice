1972185ffba794b184076f73bd919e28db2ef012
imhyunho99 <nahyunho1999@gmail.com>
2025-04-02 11:11:30 +0900

    Modi sort to quickSort

--- gitPractice/gitPractice/sort.py
+++ gitPractice/gitPractice/sort.py
@@ -1,3 +1,10 @@
-def Sort(array):
-    array = sorted(array)
-    return array
+def Sort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    pivot = arr[len(arr) // 2]  # 가운데 요소를 피벗으로 선택
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    
+    return Sort(left) + middle + Sort(right)
